0672f11fd0fdbfb1ef3ab3361b246b50276ee190
Mirko <mirko.bubba@sky.uk>
2021-03-18 09:55:34 +0000

    Added the main constrained views

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // region Views
+        val zipCodeEditText : EditText = findViewById(R.id.zipCodeEditText);
+        val enterButton : Button = findViewById(R.id.enterButton)
+        // endregion
+
+        enterButton.setOnClickListener {
+            val zipcode : String = zipCodeEditText.text.toString()
+
+            if (zipcode.length != 5 ) {
+                Toast.makeText(this, R.string.zipcode_entry_error, Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, zipcode, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
